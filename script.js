884430bbe550a5df00ddfad86d14942584463675
Luca <47353869+LeGrandMoose@users.noreply.github.com>
2024-05-17 14:33:54 +0200

    completes the challenge
    i forgot to log the stuff in the middle
    
    missing functionality - score limit at 5
    announcing winner

--- script.js
+++ script.js
@@ -1,29 +1,31 @@
+let sumComp = 0;
+let sumPlayer = 0;
 function playRound(playerSelection, computerSelection) {
     if (playerSelection.toLowerCase() == computerSelection.toLowerCase()){
-        return('This Is a draw')
+        return ['This Is a draw', 0, 0]
     }
     else if (playerSelection.toLowerCase() == 'rock'){
         if (computerSelection.toLowerCase() == 'paper'){
-            return('The computer prevails')
+            return['The computer prevails',1,0]
         }
         else if  (computerSelection.toLowerCase() == 'scissors'){
-            return('The player prevails')
+            return['The player prevails',0,1]
         }
     }
     else if (playerSelection.toLowerCase() == 'paper'){
         if (computerSelection.toLowerCase() == 'scissors'){
-            return('The computer prevails')
+            return['The computer prevails',1,0]
         }
         else if  (computerSelection.toLowerCase() == 'rock'){
-            return('The player prevails')
+            return['The player prevails',0,1]
         }
     }
     else if (playerSelection.toLowerCase() == 'scissors'){
         if (computerSelection.toLowerCase() == 'rock'){
-            return('The computer prevails')
+            return['The computer prevails',1,0]
         }
         else if  (computerSelection.toLowerCase() == 'paper'){
-            return('The player prevails')
+            return['The player prevails',0,1]
         }
     }
 
@@ -49,12 +51,50 @@ function getComputerChoice(){
     return (choices[index])   
 }
 
-const buttons = document.querySelectorAll('button');
+const buttons = document.querySelectorAll('.gameButton');
+const btn = document.querySelector('.resetButton');
+const list = document.querySelector('ul');
+
+const playVal = document.querySelector('.playerRes');
+const compVal = document.querySelector('.compRes');
+
 
 
 buttons.forEach((button) => {
 
     button.addEventListener('click', () => {
-        myValue = (playRound(button.value,getComputerChoice()))
+       let [myValue,comp,player] = (playRound(button.value,getComputerChoice()))
+        
+        sumComp = sumComp + comp;
+        sumPlayer = sumPlayer + player;
+
+        playVal.textContent = sumPlayer;
+        compVal.textContent = sumComp;
+
+        const item = document.createElement('li');
+        const spn = document.createElement('span');
+
+        spn.textContent = myValue;
+        item.appendChild(spn);
+        list.appendChild(item);
+
     });
 });
+
+btn.addEventListener('click', () =>{
+
+    list.innerHTML = ''
+    sumComp = 0;
+    sumPlayer = 0;
+
+    playVal.textContent = '0';
+    compVal.textContent = '0';
+})
+
+
+
+
+
+
+
+
